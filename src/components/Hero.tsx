91f6b891a33c05c0acaa66a8831613e213e54d74
Vignesh Venkatraman <vigneshvenkat_21@live.com>
2024-09-21 15:11:38 +0100

    Changed location of resume file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,8 +29,8 @@ const Hero = () => {
               View Skills
             </a>
             <a 
-              href="/src/public/CV-VigneshVenkatraman.pdf" 
-              download
+              href="/CV-VigneshVenkatraman.pdf" 
+              download="CV-VigneshVenkatraman.pdf"
               className="bg-transparent border-2 border-white text-white font-bold py-3 px-8 rounded-full hover:bg-white hover:text-black transition duration-300"
             >
               Download CV
