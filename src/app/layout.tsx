028e6218a1541de26eb59f9c09102337972e52c4
Vignesh Venkatraman <vigneshvenkat_21@live.com>
2024-09-23 18:03:25 +0100

    Added Favicon to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,19 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import ScrollToTop from '../components/ScrollToTop'
 
+
+import Header from '../components/Header'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Vignesh Venkatraman - Senior IT Team Lead Portfolio',
   description: 'Portfolio showcasing the projects and skills of Vignesh Venkatraman, an experienced Senior IT Team Lead',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' }
+    ]
+  }
 }
 
 export default function RootLayout({
@@ -17,7 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+      <Header />
+        <main className="pt-16">
+          {children}
+        </main>
         <ScrollToTop />
       </body>
     </html>
