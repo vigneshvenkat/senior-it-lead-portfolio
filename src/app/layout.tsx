ecc3535fcefe6147bf655d7bb78cf41ee9a6efe2
Vignesh Venkatraman <vigneshvenkat_21@live.com>
2024-09-23 18:09:32 +0100

    Removed Favicon code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,11 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import ScrollToTop from '../components/ScrollToTop'
 
-
-import Header from '../components/Header'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Vignesh Venkatraman - Senior IT Team Lead Portfolio',
-  description: 'Portfolio showcasing the projects and skills of Vignesh Venkatraman, an experienced Senior IT Team Lead',
-  icons: {
-    icon: [
-      { url: '/favicon.ico' }
-    ]
-  }
+  description: 'Portfolio showcasing the projects and skills of Vignesh Venkatraman, an experienced Senior IT Team Lead'
 }
 
 export default function RootLayout({
@@ -25,10 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header />
-        <main className="pt-16">
-          {children}
-        </main>
+        {children}
         <ScrollToTop />
       </body>
     </html>
